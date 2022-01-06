cc22394c01807be386baf7221d968c92cd1a3a99
namu10x <namphamctn1303@gmail.com>
2022-01-06 07:29:43 +0700

    Configuring React

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; //lab02_2
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
