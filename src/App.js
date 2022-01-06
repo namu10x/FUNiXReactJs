f6149967894615a16e974d029fb27e838d4309b0
namu10x <namphamctn1303@gmail.com>
2022-01-07 00:36:38 +0700

    Components Part2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';//lab02_2
 import './App.css';
 import Menu from './components/MenuComponent'
+import {DISHES} from './shared/dishes';
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    } 
+  }
   render() {
     return (
       <div>
@@ -12,7 +19,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
