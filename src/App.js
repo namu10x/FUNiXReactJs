7f809d390550d2f97fa47320015d543997f8f16a
namu10x <namphamctn1303@gmail.com>
2022-01-14 09:23:51 +0700

    Xoa 1 vai chi tiet khong dung den

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from "react";
-// import Main from './components/MainComponent';
 import { STAFFS } from "./shared/staffs";
 import "./App.css";
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { ConfigureStore } from './redux/configureStore';
 import StaffListComponent from "./components/StaffListComponent";
-// const store = ConfigureStore();
 
 class App extends Component {
   render() {
     return (
-      // <Provider store={store}>
-      // <BrowserRouter>
+      
       <div>
         {/* //truyen du lieu tu tep staff qua props */}
         <StaffListComponent staffs={STAFFS} /> 
       </div>
-      // </BrowserRouter>
-      // </Provider>
+    
     );
   }
 }
