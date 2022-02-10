3c956ee799e7229e8f9b5105a981484bfab889e1
namu10x <namphamctn1303@gmail.com>
2022-02-10 17:00:02 +0700

    Component quan ly dieu huong router

--- src/router/DieuHuongURL.js
+++ src/router/DieuHuongURL.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import BangLuong from '../components/BangLuong';
-import Home from '../components/Home';
-import NhanVien from '../components/NhanVien';
-import NhanVienChiTiet from '../components/NhanVienChiTiet';
-import PhongBan from '../components/PhongBan';
-import { STAFFS } from '../shared/staffs';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BangLuong from "../components/BangLuong";
+import Home from "../components/Home";
+import NhanVien from "../components/NhanVien";
+import NhanVienChiTiet from "../components/NhanVienChiTiet";
+import PhongBan from "../components/PhongBan";
+import { STAFFS } from "../shared/staffs";
+// Component rieng de dieu huong router
 class DieuHuongURL extends Component {
-  
-    render() {
-        return (
-       
-            <div>      
-              <hr />
-              <Switch>
-                <Route exact path="/nhanvien" >
-                <Home staffs={STAFFS}/>
-                </Route>
-                <Route exact path="/" >
-                <Home staffs={STAFFS}/>
-                </Route>
-                <Route exact path="/nhanvienchitiet/:id">
-                  <NhanVienChiTiet />
-                </Route>
-                <Route exact path="/phongban">
-                  <PhongBan />
-                </Route>
-                <Route exact path="/bangluong">
-                  <BangLuong />
-                </Route>
-              </Switch>
-              <hr />
-            </div>
-            
-       
-        );
-    }
+  render() {
+    return (
+      <div>
+        <hr />
+        <Switch>
+          <Route exact path="/nhanvien">
+            <Home staffs={STAFFS} />
+          </Route>
+          <Route exact path="/">
+            <Home staffs={STAFFS} />
+          </Route>
+          <Route exact path="/nhanvienchitiet/:id">
+            <NhanVienChiTiet />
+          </Route>
+          <Route exact path="/phongban">
+            <PhongBan />
+          </Route>
+          <Route exact path="/bangluong">
+            <BangLuong />
+          </Route>
+        </Switch>
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default DieuHuongURL;
