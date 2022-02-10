7ba5bc9768f2951763cdae7a2472b1817a0a0856
namu10x <namphamctn1303@gmail.com>
2022-02-10 17:23:03 +0700

    comment cho component PhongBan

--- src/components/PhongBan.js
+++ src/components/PhongBan.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { DEPARTMENTS } from "../shared/staffs";
+// --------presentational component--------
 function HienThiPhongBan(props) {
   return (
     <div className="col-12 col-md-6 col-lg-4">
@@ -12,7 +13,7 @@ function HienThiPhongBan(props) {
     </div>
   );
 }
-
+// --------container component--------
 class PhongBan extends Component {
   render() {
     return (
